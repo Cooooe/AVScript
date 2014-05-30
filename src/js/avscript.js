35f654d14ee3cc5e20ae7b63d144d87a8a875c1c
Coe <engen701@gmail.com>
2014-05-30 13:40:33 +0900

    adding method

--- src/js/avscript.js
+++ src/js/avscript.js
@@ -8,27 +8,187 @@
  */
 
 /**
- * 생성자
- * @param option 객체 생성 시 옵션 정의
+ * Constructor
+ * @param wrapper ::
+ * @param option ::
  * @constructor
  */
-var AVscript = function(option) {
-	this.option = {};
+var AvScript = function(wrapper, option) {
+	this._option = {};
+	this._wrapper = wrapper;
+	this._v = null;
+	this._track = null;
+	this._src = null;
+	this._rand = parseInt(Math.random() * 100000000000000, 10); // video 태그의 id값을 생성함
 
-	if ( option ) {
-		this.option = option;
+	if (option) {
+		console.log(option);
+		this.setOption(option);
 	}
 
-	this.init();
+	this._init();
 };
 
-AVscript.prototype = {
-	init : function() {
-		console.log(_U);
+AvScript.prototype = {
+
+	/**
+	 *
+	 * @private
+	 */
+	_init : function() {
 	},
-	setOption : function(option) {
-		this.option = option;
+
+	/**
+	 *
+	 * @returns {AVscript}
+	 */
+	on : function(src) {
+		if ( !this._isDuplicateVideoTag(this._rand) ) {
+			this._makeVideoTag();
+		}
+
+		if(src) {
+			this.setOption("src", src);
+		}
+
+		this.setVideoAttribute(this._option);
+
 		return this;
+	},
+
+	/**
+	 * 옵션값을 설정한다. value 값이 있을 시 option이 key값으로 기존에 있던 option에 추가된다.
+	 * @param option
+	 * @param value
+	 * @returns {AVscript}
+	 */
+	setOption : function(option, value) {
+		if ( value && typeof option.toLowerCase() === "string") {
+			this._option[option] = value;
+		} else {
+			this._option = option;
+		}
+
+		return this;
+	},
+
+	/**
+	 *
+	 * @param wrapper
+	 * @returns {AVscript}
+	 */
+	setWrapper : function(wrapper) {
+		this._wrapper = wrapper;
+		return this;
+	},
+
+	/**
+	 *
+	 * @param param
+	 * @returns {AVscript}
+	 */
+	setVideoAttribute : function(param, value) {
+		if( (typeof param).toLowerCase() === "string" && value != undefined ) {
+			var temp = {};
+
+			temp[param] = value;
+
+			param = temp;
+		}
+
+		var  _v = this._v
+			,isSrc = false
+			,isTrack = false;
+
+		for( var i in param ) {
+			switch(i) {
+				case "track" :
+					this._track = param[i];
+					isTrack = true;
+					break;
+				case "src" :
+					this._src = param[i];
+					isSrc = true;
+					break;
+				default :
+					_v.setAttribute(i, param[i]);
+					break;
+			}
+		}
+
+		if (isSrc) {
+			this.setVideoSource();
+		}
+
+		if (isTrack) {
+			this.setVideoTrack();
+		}
+
+		this._v = _v;
+
+		return this;
+	},
+
+	setVideoTrack : function() {
+		var track = this._track;
+		console.log(track);
+		if (!track) {
+			return this;
+		}
+
+		var t = document.createElement("track");
+
+		t.src = track;
+		t.srclang = "ko";
+		t.label = "korean";
+		t.default = true;
+
+		this._v.appendChild(t);
+	},
+
+	setVideoSource : function() {
+		var src = this._src;
+
+		if (!src) {
+			return this;
+		}
+
+		var s = document.createElement("source");
+
+		s.src = src;
+
+		this._v.appendChild(s);
+	},
+
+	/**
+	 *
+	 * @private
+	 */
+	_makeVideoTag : function(src) {
+		var  v = document.createElement("video")
+			,_w = this._wrapper;
+
+		this._v = v;
+
+		this.setOption("data-vid", "VID_" + this._rand);
+
+		_w.appendChild(v);
+	},
+
+	/**
+	 * check to duplicate video tag
+	 * @param id
+	 * @returns {boolean}
+	 */
+	_isDuplicateVideoTag : function(id) {
+		if ( document.querySelector("video[data-vid=VID_" + id + "]") ) {
+			return true;
+		}
+
+		return false;
+	},
+	_getVideoExtention : function(src) {
+		return src.split(".")[1];
 	}
 };
 
@@ -39,9 +199,10 @@ AVscript.prototype = {
 var _Util = function() {};
 
 _Util.prototype = {
+
 	/**
-	 * javascript 파일을 로드한다
-	 * @param jsFileName :: 파일명
+	 * load to javascript file
+	 * @param jsFileName :: javascript filename
 	 */
 	jsLoad : function(jsFileName) {
 		var s = document.createElement("script");
--- test/test.js
+++ test/test.js
@@ -5,12 +5,23 @@
  */
 
 (function() {
-	var avScript = new AVscript();
+	var avScript = new AvScript(document.querySelector("#wrapper"));
 
-	test("AVScript setOptions", function() {
+	test("AVScript", function() {
 		var now = "2014/05/29 10:35:00";
 		ok(avScript.setOption({
 			"duration" : 100
 		}), "setOption");
+
+		ok(avScript.setWrapper(document.querySelector("#wrapper")), "setWrapper");
+
+		ok(avScript.on(), "on");
+
+		ok(avScript.setVideoAttribute({
+			"auto-play" : true
+		}), "setAttribute (Auto-play)");
 	});
+
+
+	console.log(avScript);
 })();
